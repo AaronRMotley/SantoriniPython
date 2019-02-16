fea57ec2c75fc6b425ed889928f578c2bfb26feb
Aaron Motley <aaronrmotley@gmail.com>
2019-02-15 20:19:32 -0600

    player class added

--- Player.py
+++ Player.py
@@ -1,8 +1,11 @@
 import Worker as w
 
 class Player: 
-    def __init__(self, god):
+    def __init__(self, god, color):
         self.workers = []
+        self.god = god
+        self.color = color
 
-    def addworker(self):
+    def add_worker(self):
         self.workers.append(w.Worker())
+        
