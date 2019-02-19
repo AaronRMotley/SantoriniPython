ba190a620e1a4a6d3f4365bd85c29763ffd59526
Aaron Motley <aaronrmotley@gmail.com>
2019-02-18 19:34:54 -0600

    refactor one stable

--- Board.py
+++ Board.py
@@ -1,5 +1,3 @@
-import CellState as cs
-import Worker as wk
 import Player as pl
 
 # BoardPieces
@@ -7,19 +5,21 @@ LEVEL_ONE,LEVEL_TWO,LEVEL_THREE,DOMES = 22,18,14,18
 WORKER_MAX = 2
 # Other stats
 PRINT_SPACING = 5
+NULL = ''
 
 class BoardManager:
     def __init__(self, board_size = 5):
-        self.board = [[(cs.Cell(), '') for n in range(board_size)] for x in range(board_size)]
+        self.board = [[(Cell()) for n in range(board_size)] for x in range(board_size)]
         for r in range(len(self.board)):
             for c in range(len(self.board)):
                 if r == 0 or r == (board_size-1) or c == 0 or c == (board_size-1):
-                    self.board[c][r][0].perimeter = True
-    
+                    self.board[c][r].perimeter = True
+
     def add_worker(self, worker, row, column):
-        if self.board[row-1][column-1][0].occupied != True:
-            self.board[row-1][column-1] = (self.board[row-1][column-1][0], worker)
-            self.board[row-1][column-1][0].occupied = True
+        if row < 1 or row > 5 or column < 0 or column > 5:
+            return "invalid inputs"
+        if self.board[row-1][column-1].worker == NULL:
+            self.board[row-1][column-1].worker = worker
         else:
             return "Space is occupied"
 
@@ -41,11 +41,30 @@ class BoardManager:
         for r in self.board:
             s += '\n'+str(row)+(" "*PRINT_SPACING)
             for c in r:
-                s += "L"+str(c[0].level)
-                if c[0].occupied == True:
-                    s+= "-"+str(c[1]) + str(c[1]) +(" "*(PRINT_SPACING-2))
+                s += "L"+str(c.level)
+                if c.worker != NULL:
+                    s+= "-"+str(c.worker.color) + str(c.worker.id) +(" "*(PRINT_SPACING-3))
                 else:
                     s += (" "*PRINT_SPACING)
             #s += '\n'
             row += 1
         return s
+
+
+class Cell:
+    def __init__(self, perimeter = False):
+        self.level = 0
+        self.dome = False
+        self.perimeter = perimeter
+        self.worker = NULL
+        
+    def build(self):
+        if self.level < 4:
+            self.level += 1
+        if self.level >= 4:
+            self.dome = True
+
+    # Used only by God of war
+    def destroy(self):
+        if self.level > 0 and self.dome != True:
+            self.level -= 1
--- Main.py
+++ Main.py
@@ -1,29 +1,21 @@
 #import tkinter as tk
-import Worker as wk
-import CellState as cs
 import Board as bd
 import Player as pl
 
 PLAYER_COUNT = 2
 
 main = bd.BoardManager()
-player1 = pl.Player("G")
-for x in range(PLAYER_COUNT):
-    player1.workers.append(wk.Worker())
-player2 = pl.Player("R")
-for x in range(PLAYER_COUNT):
-    player2.workers.append(wk.Worker())
-print(main)
 
-print(player1.workers[0])
+player1 = pl.Player("G")
+player2 = pl.Player("R")
 
 main.add_worker(player1.workers[0], 1, 1)
-print(main)
-main.add_worker(player1.workers[1], 1, 2)
-print(main)
-main.add_worker(player2.workers[0], 3, 4)
-print(main)
+main.add_worker(player1.workers[1], 3, 5)
+main.add_worker(player2.workers[0], 2, 2)
 main.add_worker(player2.workers[1], 5, 5)
+
+print(main)
+
 
 
 
